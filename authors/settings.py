ab4899607cdbe009ffcc7cd85b4d15b4ebd61149
KE-C02SY35BGY25 <ke-c02sy35bgy25@Ians-MacBook-Pro.local>
2018-09-25 10:37:38 +0300

    [CHORE #160577465] Remove commented out code

--- authors/settings.py
+++ authors/settings.py
@@ -133,26 +133,6 @@ CORS_ORIGIN_WHITELIST = (
 # called `INSTALLED_APPS`.
 AUTH_USER_MODEL = 'authentication.User'
 
-# REST_FRAMEWORK = {
-#     'EXCEPTION_HANDLER': 'authors.apps.core.exceptions.core_exception_handler',
-#     'NON_FIELD_ERRORS_KEY': 'error',
-
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'authors.apps.authentication.backends.JWTAuthentication',
-#     ),
-# }
-
-# REST_FRAMEWORK = {
-#    # When you enable API versioning, the request.version attribute will contain a string
-#    # that corresponds to the version requested in the incoming client request.
-#    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
-#    'DEFAULT_AUTHENTICATION_CLASSES': (
-#        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-#    ),
-#    'DEFAULT_PERMISSION_CLASSES': (
-#        'rest_framework.permissions.IsAuthenticated',
-#     )
-# }
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
