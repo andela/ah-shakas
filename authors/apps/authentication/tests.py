13ae36fa93b615960c9254e4770bc96efe608046
gloriaodipo <gloriaodipo@gmail.com>
2018-09-28 00:09:23 +0300

    Add docstrings to all tests

--- authors/apps/authentication/tests.py
+++ authors/apps/authentication/tests.py
@@ -8,7 +8,13 @@ from .models import UserManager
 
 
 class AuthenticationTest(APITestCase):
+    """
+    User authentication test cases
+    """
     def setUp(self):
+        """
+        Method for setting up user
+        """
         self.url = api_reverse('authentication:user-registration')
         self.user =  { 
             'user' : { 
@@ -19,29 +25,44 @@ class AuthenticationTest(APITestCase):
         }
 
     def test_user_can_signup(self):
+        """
+        Test API can successfully register a new user
+        """
         response = self.client.post(self.url, self.user, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIn(b'jane@doe.com', response.content)
     
     def test_user_cannot_signup_without_email(self):
+        """
+        Test API cannot register user without email
+        """
         del self.user['user']['email']
         response = self.client.post(self.url, self.user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn(b'Email is required', response.content)
 
     def test_user_cannot_signup_with_invalid_email(self):
+        """
+        Test API cannot register user with an invalid email format
+        """
         self.user['user']['email'] = 'NOT-AN-EMAIL'
         response = self.client.post(self.url, self.user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn(b'Enter a valid email address', response.content)
 
     def test_user_cannot_signup_without_password(self):
+        """
+        Test API cannot register user without password
+        """
         del self.user['user']['password']
         response = self.client.post(self.url, self.user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn(b'Password is required', response.content)
 
     def test_user_cannot_signup_with_short_password(self):
+        """
+        Test api cannot register user with a password of less than 8 characters
+        """
         self.user['user']['password'] = 'abc'
         response = self.client.post(self.url, self.user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -49,6 +70,9 @@ class AuthenticationTest(APITestCase):
                 response.content)
 
     def test_user_cannot_signup_without_alphanumeric_password(self):
+        """
+        Test API cannot register user with a non-alphanumeric password
+        """
         self.user['user']['password'] = 'abcdefghijk'
         response = self.client.post(self.url, self.user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -56,12 +80,18 @@ class AuthenticationTest(APITestCase):
                 response.content)
 
     def test_user_cannot_signup_without_username(self):
+        """
+        Test API cannot register user without a username
+        """
         del self.user['user']['username']
         response = self.client.post(self.url, self.user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn(b'Username is required', response.content)
 
     def test_user_cannot_signup_with_short_username(self):
+        """
+        Test api cannot register user with username of less than 4 characters
+        """
         self.user['user']['username'] = 'abc'
         response = self.client.post(self.url, self.user, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
