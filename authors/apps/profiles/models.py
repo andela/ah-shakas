6b18eda28e526d6eccb54a2edd8cca1aca4e8b92
KE-C02SY35BGY25 <ke-c02sy35bgy25@Ians-MacBook-Pro.local>
2018-10-02 10:08:54 +0300

    Fix syntax error in the models file

--- authors/apps/profiles/models.py
+++ authors/apps/profiles/models.py
@@ -17,7 +17,6 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL):
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
-    
