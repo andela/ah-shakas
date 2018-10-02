943225135ad85c7fc446c01b83ed622adfd096ae
KEVIN KOECH <koech@KEVINs-MBP.mshome.net>
2018-10-02 23:33:40 +0300

    [Feature #160577477]users can receive links via  emails to reset password

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -14,6 +14,11 @@ urlpatterns = [
     path('user/', UserRetrieveUpdateAPIView.as_view()),
     path('users/', RegistrationAPIView.as_view(), name='user-registration'),
     path('users/login/', LoginAPIView.as_view()),
+<<<<<<< HEAD
     path('users/email_sent', EmailSentAPIView.as_view(), name='email_password'),
     path('users/password_reset', PasswordResetAPIView.as_view(), name='password_reset'),
+=======
+    path('users/email_sent', EmailSentAPIView.as_view()),
+    path('users/password_reset', PasswordResetAPIView.as_view()),
+>>>>>>> [Feature #160577477]users can receive links via  emails to reset password
 ]
