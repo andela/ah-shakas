78fc5c6884c2a4b20af973de0535ad7f4466de09
KE-C02SY35BGY25 <ke-c02sy35bgy25@Ians-MacBook-Pro.local>
2018-10-02 10:32:02 +0300

    Add get and put method in the ProfileAPIView

---
+++ authors/apps/profiles/views.py
@@ -0,0 +1,39 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+# Create your views here.
+
+class ProfileAPIView(APIView):
+    #Allow any user to hit this endpoint
+    permission_classes = (AllowAny,)
+    renderer_classes = (ProfileJSONRenderer,)
+
+    def get(self, request, username, format=None):
+        try:
+            profile =  Profile.objects.get(user__username=username)
+            serializer = ProfileSerializer(profile)
+            return Response(serializer.data)
+        except Profile.DoesNotExist:
+            return Response(
+                {
+                    'message': 'Profile not found'
+                }, 
+                status=status.HTTP_404_NOT_FOUND
+            )
+    
+    def put(self, request, username, format=None):
+        try:
+            profile = Profile.objects.get(user__username=username)
+            serializer_data = request.data.get('user', {})
+            serializer = ProfileSerializer(profile, data=serializer_data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        except Profile.DoesNotExist:
+            return Response(
+                    {
+                        'message': 'Profile not found'
+                    }, 
+                    status=status.HTTP_404_NOT_FOUND
+                )
