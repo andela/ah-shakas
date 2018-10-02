417e31caf0f6e683e38b01fd7f2d3caa0b7df3f6
KE-C02SY35BGY25 <ke-c02sy35bgy25@Ians-MacBook-Pro.local>
2018-10-02 09:32:41 +0300

    Create Profile model with relationship to the User model

---
+++ authors/apps/profiles/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from authors import settings
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=255, default='Update your bio')
+    image_url = models.CharField(max_length=255)
+    following = models.BooleanField(default=False)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL):
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+    
