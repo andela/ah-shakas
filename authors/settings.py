52386a3da7d1e3934604c4aad83bd89e28eb9a02
gloriaodipo <gloriaodipo@gmail.com>
2018-10-03 11:05:33 +0300

    Add test for token generation

--- authors/settings.py
+++ authors/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'PASSWORD':os.getenv('DB_PASSWORD'),
         'HOST':os.getenv('DB_HOST'),
