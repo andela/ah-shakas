e4196bd5e510ea494ddbc48d737b48320218024b
Kelvin Lukorito <kelvin.lukorito@andela.com>
2018-10-03 19:33:23 +0300

    Add article CRUD methods

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -2,16 +2,14 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework import status
+from .permissions import IsOwnerOrReadonly
 
 from .permissions import IsOwnerOrReadonly
 from .models import ArticlesModel
 from .serializers import ArticlesSerializers
 
 
-
 class ArticlesList(generics.ListCreateAPIView):
-    queryset = ArticlesModel.objects.all()
-    serializer_class = ArticlesSerializers
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def post(self, request):
