ea9836966f90f26d8f6656770a187628223f412b
Kelvin Lukorito <kelvin.lukorito@andela.com>
2018-10-03 11:17:43 +0300

    [Feature #160577480] Initial Commit

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 from .models import ArticlesModel
+<<<<<<< HEAD
 from authors.apps.authentication.serializers import UserSerializer
 from authors.apps.articles.helpers import get_time_to_read_article
 
 
 class ArticlesSerializers(serializers.ModelSerializer):
 
+=======
+
+class ArticlesSerializers(serializers.ModelSerializer):
+    # author = serializers.CharField(source='author.username')
+>>>>>>> [Feature #160577480] Initial Commit
     title = serializers.CharField(
         required=True,
         max_length=128,
