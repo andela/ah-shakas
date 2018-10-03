fae9ad02afaf935f39a285def09b2b312c8fa6c6
KE-C02SY35BGY25 <ke-c02sy35bgy25@Ians-MacBook-Pro.local>
2018-10-03 12:19:53 +0300

    Add the url name to redirect to a specific view

--- authors/apps/profiles/urls.py
+++ authors/apps/profiles/urls.py
@@ -6,7 +6,7 @@ from .views import ProfileAPIView
 app_name = "profiles"
 
 urlpatterns = [
-    path('profiles/<username>/', ProfileAPIView.as_view())
+    path('profiles/<username>/', ProfileAPIView.as_view(), name='user-profile')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
