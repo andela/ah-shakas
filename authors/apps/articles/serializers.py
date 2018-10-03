ce7c8492d4564bb48312e1701441edc5b5145197
Kelvin Lukorito <kelvin.lukorito@andela.com>
2018-10-03 11:17:43 +0300

    [Feature #160577480] Initial Commit

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -5,7 +5,6 @@ from authors.apps.profiles.serializers import ProfileSerializer
 from .models import ArticlesModel, Comment
 
 
-
 class ArticlesSerializers(serializers.ModelSerializer):
     title = serializers.CharField(
         required=True,
