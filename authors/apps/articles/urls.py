8aa573d27687a85164dbe0237083ff8e76623fd5
gloriaodipo <gloriaodipo@gmail.com>
2018-10-04 14:47:21 +0300

    [FEATURE #160577482] Add urls for comments

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -5,6 +5,11 @@ from . import views
 app_name = "articles"
 
 urlpatterns = [
+<<<<<<< HEAD
+=======
+    path('articles/', views.ArticlesList.as_view(), name='articles'),
+    path('articles/<slug>', views.ArticlesDetails.as_view(),  name='article-details')
+>>>>>>> [FEATURE #160577482] Add urls for comments
     path('articles/', views.ArticlesList.as_view()),
     path('articles/<slug>', views.ArticlesDetails.as_view()),
     path('articles/<slug>/comments/', views.CommentsListCreateView.as_view())
