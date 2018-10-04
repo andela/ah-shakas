0fa6419d3f39150fa7d24132ac34ece7c4d6a4a2
Kelvin Lukorito <kelvin.lukorito@andela.com>
2018-10-04 18:53:29 +0300

    separate local relative imports

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -4,12 +4,6 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .permissions import IsOwnerOrReadonly
-from .models import ArticlesModel
-from .serializers import ArticlesSerializers
-
-
-
-from .permissions import IsOwnerOrReadonly
 from .models import ArticlesModel, Comment
 from .serializers import ArticlesSerializers, CommentsSerializers
 from .renderers import ArticlesRenderer
