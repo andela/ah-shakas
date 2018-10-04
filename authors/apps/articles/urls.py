71dc01bf4509431231608668b42528f8c9947097
Kelvin Lukorito <kelvin.lukorito@andela.com>
2018-10-04 18:52:35 +0300

    Add name to article url

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from . import views
 
 app_name = "articles"
 
 urlpatterns = [
-    path('articles/', views.ArticlesList.as_view()),
-    path('articles/<slug>', views.ArticlesDetails.as_view())
+    path('articles/', views.ArticlesList.as_view(), name='articles'),
+    path('articles/<slug>', views.ArticlesDetails.as_view(),  name='article-details')
 ]
