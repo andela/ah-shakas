755489b0dff5557223e3dc62c733384b4b8c1183
Kelvin Lukorito <kelvin.lukorito@andela.com>
2018-10-04 18:53:29 +0300

    separate local relative imports

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -2,7 +2,6 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework import status
-from .permissions import IsOwnerOrReadonly
 
 
 from .permissions import IsOwnerOrReadonly
