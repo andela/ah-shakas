172e6a885f1147523cea107ca110ebe016ebfa6e
KEVIN KOECH <koech@KEVINs-MacBook-Pro.local>
2018-10-05 16:26:54 +0300

    [Feature #160577477] implement PO feedback

--- authors/apps/authentication/test_password_reset.py
+++ authors/apps/authentication/test_password_reset.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.reverse import reverse as api_reverse
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from .models import User
+from .password_token import generate_password_token
 
 class TestEmailSent(APITestCase):
     """
@@ -19,7 +20,7 @@ class TestEmailSent(APITestCase):
             'user' : {
                 'username': 'kevin',
                 'email': 'koechkevin92@gmail.com',
-                'password': 'Kev12345'
+                'password': 'Kevin12345'
             }
         }
         self.client.post(self.register_url, self.user, format="json")
@@ -69,23 +70,24 @@ class TestPasswordReset(APITestCase):
             'user' : {
                 'username': 'kevin',
                 'email': 'koechkevin92@gmail.com',
-                'password': 'Kev12345'
+                'password': 'Kevin12345'
             }
         }
         self.register_url = api_reverse('authentication:user-registration')
         self.client.post(self.register_url, self.user, format="json")
         User.is_active = True
+        email='koechkevin92@gmail.com'
+        user_object = User.objects.filter(email=email).first()
         token_generator = PasswordResetTokenGenerator()
-        user_object = User.objects.filter(email='koechkevin92@gmail.com').first()
-        token = token_generator.make_token(user_object)
+        raw_token = token_generator.make_token(user_object)
+        token = generate_password_token(email, raw_token)
         self.url = api_reverse('authentication:password_reset')+'?token='+token
-        #self.url = 'http://127.0.0.1:8000/api/users/password_reset?token='+token
 
     def test_invalid_token(self):
         """
         test a case where an invalid token is provided or token has been tampered with
         """
-        response = self.client.put(self.url+'r', data={"email":"koechkevin92@gmail.com"})
+        response = self.client.put(self.url+'r', data={})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.content, b'{"user": {"message": "invalid token"}}')
 
@@ -93,10 +95,7 @@ class TestPasswordReset(APITestCase):
         """
         test a case where valid token is provided
         """
-        data = {
-            "email":"koechkevin92@gmail.com",
-            "password":"Kev12345"
-        }
+        data = {"password":"Kev12345"}
         response = self.client.put(self.url, data=data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.content, b'{"user": {"message": "password successfully changed"}}')
@@ -105,18 +104,13 @@ class TestPasswordReset(APITestCase):
         """
         a case where user tries to reuse a token
         """
-        data = {
-            "email":"koechkevin92@gmail.com",
-            "password":"Kev12345"
-        }
-        data2 = {
-            "email":"koechkevin92@gmail.com",
-            "password":"Kevinkoech"
-        }
+        data = {"password":"Kev12345"}
+        data2 = {"password":"Kevinkoech"}
         response1 = self.client.put(self.url, data=data)
         response2 = self.client.put(self.url, data=data2)
         self.assertEqual(response1.status_code, status.HTTP_200_OK)
-        self.assertEqual(response1.content, b'{"user": {"message": "password successfully changed"}}')
+        self.assertEqual(response1.content,
+        b'{"user": {"message": "password successfully changed"}}')
         self.assertEqual(response2.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response2.content, b'{"user": {"message": "invalid token"}}')
 
@@ -124,10 +118,42 @@ class TestPasswordReset(APITestCase):
         """
         a case where user provides a short password on reset
         """
-        data = {
-            "email":"koechkevin92@gmail.com",
-            "password":"Kev123"
-            }
+        data = {"password":"Kev123"}
         response = self.client.put(self.url, data=data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.content, b'{"user": {"password": ["Password must have at least 8 characters", "Password must have a number and a letter"]}}')
+        self.assertEqual(response.content,
+         b'{"user": {"password": ["Password must have at least 8 characters", "Password must have a number and a letter"]}}')
+
+    def test_login_new_password(self):
+        """
+        test login new password
+        """
+        data = {"password":"Kibitok92"}
+        login_data = self.user =  {
+            'user' : {
+                'email': 'koechkevin92@gmail.com',
+                'password': 'Kibitok92'
+            }
+        }
+        self.client.put(self.url, data=data)
+        login_url = api_reverse('authentication:user_login')
+        response = self.client.post(login_url, login_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_old_password(self):
+        """
+        old password can no longer be used by the user
+        """
+        data = {"password":"Kibitokkoech90"}
+        login_url = api_reverse('authentication:user_login')
+        login_data = self.user =  {
+            'user' : {
+                'email': 'koechkevin92@gmail.com',
+                'password': 'Kevin12345'
+            }
+        }
+        self.client.put(self.url, data=data)
+        response = self.client.post(login_url, login_data, format='json')
+        self.assertEqual(response.content,
+        b'{"user": {"non_field_errors": ["A user with this email and password was not found."]}}')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
