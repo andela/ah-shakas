a45607dc3dce933267ed7b13a43226b2ed0a522c
Kelvin Lukorito <kelvin.lukorito@andela.com>
2018-10-09 11:44:55 +0300

    Modify imports according to feedback

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework import status
@@ -23,7 +23,7 @@ class ArticlesList(generics.ListCreateAPIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class ArticlesDetails(generics.RetrieveUpdateDestroyAPIView):
+class ArticlesDetails(RetrieveUpdateDestroyAPIView):
     queryset = ArticlesModel.objects.all()
     serializer_class = ArticlesSerializers
     renderer_classes = (ArticlesRenderer,)
