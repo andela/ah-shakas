1a706889a9b0fa4e49cf3838718dc6624f11700f
gloriaodipo <gloriaodipo@gmail.com>
2018-10-09 20:47:10 +0300

    Add custom representation for returning time taken to read

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -3,7 +3,6 @@ from .models import ArticlesModel
 from authors.apps.authentication.serializers import UserSerializer
 from authors.apps.articles.helpers import get_time_to_read_article
 
-
 class ArticlesSerializers(serializers.ModelSerializ
 
     title = serializers.CharField(
