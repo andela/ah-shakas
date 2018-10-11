a2ceba1692856be3fc712ceecfddf83ff8e7373b
michael-basweti <baswetima@gmail.com>
2018-10-11 10:25:53 +0300

    update tests

--- authors/apps/authentication/test_social_auth.py
+++ authors/apps/authentication/test_social_auth.py
@@ -29,13 +29,5 @@ class SocialAuthSignUp(APITestCase):
         resp=self.client.post(self.social_url,data=data)
         self.assertEqual(resp.status_code,status.HTTP_400_BAD_REQUEST)
 
-    def test_for_passing_token(self):
-        provider = 'facebook'
-        access_token = 'EAAe6UMnIZBRoBAPXW605GDyMZB3RaLvgJAsi1fRaQM3kmSkQ4ZACNnz2ZC5fRvdqSFT7njB908l4XTNR1pZCaWlx8xzCzMV0lmtVudscfBIwBIPiQUXzFy5XWVA1iHgnjgFA3vBdQvEo4Tm6YPlCaEIUhQzdDz1Lh6SAC7psV0wsNkqcSdemDLk8yH9L9MFi5u1Ot075vIir3A9fc4FAmzqio9e6W16edB4ZB0ihZAra0rY0XZBWkQgH'
-        data={"provider":provider,"access_token":access_token}
-        resp=self.client.post(self.social_url,data=data)
-        self.assertEqual(resp.status_code,201)
-
-    
 
     
