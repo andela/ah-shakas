40382de822534048c166e36ee45002a28491f32f
gloriaodipo <gloriaodipo@gmail.com>
2018-10-12 14:48:51 +0300

    Change return statement for time taken to read article

--- authors/apps/articles/helpers.py
+++ authors/apps/articles/helpers.py
@@ -7,4 +7,6 @@ def get_time_to_read_article(article, words_per_minute=250):
     '''
     body = article.body
     word_count = len(re.sub('[^A-Za-z0-9]+', ' ', body).split())
-    return f"{ceil(word_count/words_per_minute)} mins"
+    if ceil(word_count/words_per_minute) == 1:
+        return f"{ceil(word_count/words_per_minute)} min"
+    return f"{ceil(word_count/words_per_minute)} mins"    
